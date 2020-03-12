c6a5b2de9d9e38669be97ac3e50ffa6a7d5651be
Bao Nguyen <bao.nguyen@logixtek.com>
2020-03-12 16:49:04 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import * as tf from '@tensorflow/tfjs';
+
+
+class PolicyNetwork {
+    
+    constructor(num_actions) {
+        this.num_actions = num_actions;
+        this.createPolicyNetwork([4, 4]);
+    }
+
+    /**
+     * Create the underlying model of this policy network.
+     * @param {*} hiddenLayerSize 
+     */
+    createPolicyNetwork(hiddenLayerSizes) {
+        this.policyNet = tf.sequential();
+        
+        // add input layer
+        this.policyNet.add(tf.layers.dense({units: 4, inputShape=(this.num_actions)}));
+
+        // add hidden layers
+        hiddenLayerSizes.forEach((num_unit, i) => {
+            this.policyNet.add(tf.layers.dense({
+                units: num_unit,
+                activation: 'relu'
+            }));
+        });
+
+        // output layer
+        this.policyNet.add(tf.layers.dense({units: 1}));
+    }
+}
